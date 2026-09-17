4ca6f5ed4b3ad5819ff6f4ca1c678c66bd7b9785
inmontse-cloud <gzvanessatse@gmail.com>
2026-09-17 08:42:06 +0000

    [skip actions] lab/lab02/ch02_t03_escaping_characters.py

--- lab/lab02/ch02_t03_escaping_characters.py
+++ lab/lab02/ch02_t03_escaping_characters.py
@@ -2,4 +2,3 @@
 # Uncomment the following line!
 # 'This isn't flying, this is falling with style!'
 print("This isn\'t flying, this is falling with style!")
-print("This isn't flying, this is falling with style!")
