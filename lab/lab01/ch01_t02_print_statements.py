4c991ea02efdbf56a68eb99c29c4d70561b056d8
inmontse-cloud <gzvanessatse@gmail.com>
2026-09-17 07:32:23 +0000

    [skip actions] lab/lab01/ch01_t02_print_statements.py

--- lab/lab01/ch01_t02_print_statements.py
+++ lab/lab01/ch01_t02_print_statements.py
@@ -1,2 +1,2 @@
 
-print("Deep into distant woolands winds a mazy way, reaching to overlepping apurs of mountains bathed in their hill-side blue.)
+print("Deep into distant woolands winds a mazy way, reaching to overlepping apurs of mountains bathed in their hill-side blue.")
