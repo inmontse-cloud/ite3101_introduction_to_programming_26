128ef39a2898d9cca6109525d543713f3ce6928c
inmontse-cloud <gzvanessatse@gmail.com>
2026-09-17 08:37:07 +0000

    [skip actions] lab/lab02/ch02_t02_practice.py

--- lab/lab02/ch02_t02_practice.py
+++ lab/lab02/ch02_t02_practice.py
@@ -1,6 +1,7 @@
 # Assign your variables below, each on its own line!
 caesar = "caesar!"
-
+caesar = "caesar!"
+caesar = "caesar!"
 # Put your variables above this line, and uncomment the following 3 line of codes.
 # print(caesar)
 # print(praline)
